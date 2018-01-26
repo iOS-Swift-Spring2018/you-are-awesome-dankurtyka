af13caf377fc803bba2e57082f707b9eb1ed88b5
Dan Kurtyka <danielkurtyka@kurtykadmac.bc.edu>
2018-01-26 18:39:34 -0500

    You Are Awesome Commit

--- You are Awesome!/ViewController.swift
+++ You are Awesome!/ViewController.swift
@@ -9,17 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    var index = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    @IBAction func showMessagePressed(_ sender: UIButton) {
+        
+        let messages = ["you Are Fantastic!",
+                        "You Are Great!",
+                        "You Are Amazing!",
+                        "When the Genius Bar needs help, they call you!",
+                        "You Brighten My Day!",
+                        "You Are Da Bomb!",
+                        "I can't wait to use your app!"]
+        
+        messageLabel.text = messages[index]
+        index += 1
+        
+        if index == messages.count {
+            index = 0
+            
+        }
+        
+//        let message1 = "You Are Da Bomb!"
+//        let message2 = "You Are Great!"
+//        let message3 = "You Are Amazing"
+//
+//        if messageLabel.text == message1 {
+//            messageLabel.text = message2
+//        } else if messageLabel.text == message2 {
+//            messageLabel.text = message3
+//        } else {
+//            messageLabel.text = message1
+//        }
+//
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
