273886c6334f15f4b816ba811f02b796eb20f0a8
Dan Kurtyka <danielkurtyka@kurtykadmac.bc.edu>
2018-01-29 18:48:27 -0500

    1 - 2.15

--- You are Awesome!/ViewController.swift
+++ You are Awesome!/ViewController.swift
@@ -7,15 +7,48 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var awesomeImage: UIImageView!
     @IBOutlet weak var messageLabel: UILabel!
-    var index = 0
+    var awesomePlayer = AVAudioPlayer()
+    var index = -1
+    var imageNumber = -1
+    var soundNumber = -1
+    let numberOfImages = 7
+    let numberOfSounds = 3
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    }
+    
+    func playSound(soundName: String) {
+        // can we load in the file soundName?
+        if let sound = NSDataAsset(name: soundName) {
+            // check if sound.data is a sound file
+            do {
+                try awesomePlayer = AVAudioPlayer(data: sound.data)
+                awesomePlayer.play()
+            } catch {
+                // if sound.data is not a valid audio file
+                print("ERROR: data in \(soundName) couldn't be played as a sound.")
+                
+            }
+        } else {
+            // if reading in the NSDataAsset didn't wor, tell the user /report the error.
+            print ("ERROR: file \(soundName) didn't load")
+        }
+    }
+    
+    func nonRepeatingRandom(lastNumber: Int, maxValue: Int) -> Int {
+        var newIndex = -1
+        repeat {
+            newIndex = Int(arc4random_uniform(UInt32(maxValue)))
+        }while lastNumber == newIndex
+        return newIndex
     }
     
     @IBAction func showMessagePressed(_ sender: UIButton) {
@@ -26,15 +59,43 @@ class ViewController: UIViewController {
                         "When the Genius Bar needs help, they call you!",
                         "You Brighten My Day!",
                         "You Are Da Bomb!",
-                        "I can't wait to use your app!"]
+                        "I can't wait to use your app!",
+                        "Fabulous? That's You!"]
         
+        
+        // show a message
+        index = nonRepeatingRandom(lastNumber: index, maxValue: messages.count)
         messageLabel.text = messages[index]
-        index += 1
         
-        if index == messages.count {
-            index = 0
-            
-        }
+        // show an image
+        awesomeImage.isHidden = false
+
+        imageNumber = nonRepeatingRandom(lastNumber: imageNumber, maxValue: numberOfImages)
+        awesomeImage.image = UIImage(named: "image\(imageNumber)")
+        
+        
+        // Get a random number to use in our soundName file
+
+        
+        soundNumber = nonRepeatingRandom(lastNumber: soundNumber, maxValue: numberOfSounds)
+        
+        // play a sound
+        let soundName = "sound\(soundNumber)"
+        playSound(soundName: "sound2")
+
+        
+//        var randomIndex = Int(arc4random_uniform(UInt32(messages.count)))
+//        messageLabel.text = messages[randomIndex]
+        
+        
+        
+//        messageLabel.text = messages[index]
+//        index += 1
+//
+//        if index == messages.count {
+//            index = 0
+//
+//        }
         
 //        let message1 = "You Are Da Bomb!"
 //        let message2 = "You Are Great!"
